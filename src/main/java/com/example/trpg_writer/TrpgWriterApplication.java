7699912fb5015f810cd6994d0dca59d249c2ac89
Takaichi-kouya <kou.chocobo@gmail.com>
2025-07-06 18:44:39 +0900

    first

---
+++ src/main/java/com/example/trpg_writer/TrpgWriterApplication.java
@@ -0,0 +1,13 @@
+package com.example.trpg_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrpgWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrpgWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trpg_writer/TrpgWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trpg_writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrpgWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
