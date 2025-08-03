1f0d225ee5e8ef75867a98e3ad48c09a9949e5a6
Takaichi-kouya <kou.chocobo@gmail.com>
2025-08-03 14:20:50 +0900

    ホーム画面更新　ハリボテからある程度形に　ロゴ追加

--- src/main/java/com/example/trpg_writer/security/WebSecurityConfig.java
+++ src/main/java/com/example/trpg_writer/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/login", "/signup").permitAll()  // すべてのユーザーにアクセスを許可するURL
+                .requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/login", "/signup", "/faqs/**").permitAll()  // すべてのユーザーにアクセスを許可するURL
                 .anyRequest().authenticated()                   // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
             )
             .formLogin((form) -> form
